b9d9dce18510b9adbe4b5c593be27aa16e541eec
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-04-27 14:09:30 +0300

    Add files via upload

---
+++ Info/cypress_info.cy.js
@@ -0,0 +1,32 @@
+// ----------------Подсказка - //
+/// <reference types = "cypress" />    
+
+// ----------------------------- Комады для  cypress ---------------------------
+// npm install cypress --save-dev
+// npx cypress open
+// --------------------------------- ТЕСТЫ ------------------------------------
+describe('first test', () =>{
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/')                    // Перейти на страницу - вынесли в общее для всех тестов
+    })
+    it.only ('verify link', () => {                                // Если нужно пройти только один тест - only (it.only)
+        //cy.visit('https://example.cypress.io/')                  // Перейти на страницу
+        cy.contains ('next').click()                               // Найти ссылку с текстом 'next' и кликнуть на нее
+        cy.url().should('include', '/commands/traversal')          // Найти url, кот. должен включать в себя ('include') часть ссылки '/commands/traversal'
+        cy.get('div.container h1').should('have.text', 'Traversal')// Обратиться к элементу через локатор(CSS) 'div.container h1'и проверить текст заголовка 'Traversal'
+    })
+
+
+    it.skip ('verify link2', () => {                               // Если нужно пропустить тест - skip (it.skip)
+        //cy.visit('https://example.cypress.io/')                   
+        cy.contains ('assert').click()                               
+        //cy.url().should('include', '/commands/assertions')      // Завалит тест, т.к. не соответствие заголовку    
+    })
+
+    it ('verify link2', () => {
+        //cy.visit('https://example.cypress.io/')                          
+        // Зайти на данную страницу
+        cy.get('a[href="/commands/actions"]').contains('select').click() // Перейти по ссылке (href=), а именно (contains) 'select' и кликнуть на нее                            
+        cy.url().should('include', '/commands/actions')                  // Найти url, включ.в себя '/commands/actions'
+    })
+})
