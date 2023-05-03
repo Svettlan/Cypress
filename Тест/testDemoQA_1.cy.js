3c405dcea372c7a80b64d89c04a66f6f7b16e27b
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-05-03 14:45:04 +0300

    Update testDemoQA_1.cy.js

--- Тест/testDemoQA_1.cy.js
+++ Тест/testDemoQA_1.cy.js
@@ -7,13 +7,21 @@ const expectedMenuItemNames = [
    'Interactions',
    'Book Store Application".
 ];
-it("Verification menu item names', function () {
+it("Verification menu item names - Variant 1', function () {
+  cy.visit('https://demoga.com");                    //
+  cy.get('.card')
+    .should have.length', expectedMenuItemNames.length)
+    .each((el, idx) => {
+      cy.log(el) 
+      expect(el.text()).to.be.equal(expectedMenuItemNames[idx]);
+    });
+  })
+it("Verification menu item names - Variant 2', function () {
   cy.visit('https://demoga.com");
   cy.get('.card')
-    should have.length', expectedMenuItemNames.length)
-   .each((el, idx) => {
-     cy.log(el) 
-     expect(el.text()).to.be.equal(expectedMenuItemNames[idx]);
+    .should have.length', expectedMenuItemNames.length)
+    .then (($els) =>{
+     cy.log(Cypress.$.makeArray($els).map
     });
-})
+  })
 })
