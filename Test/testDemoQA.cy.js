3749fc13e63f19485451adfdc2633c6264da38a6
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-05-09 16:28:52 +0300

    Update testDemoQA.cy.js

--- Test/testDemoQA.cy.js
+++ Test/testDemoQA.cy.js
@@ -7,6 +7,24 @@ describe('first test', () =>{
     it('verify link1', () => {                                
         cy.get('div.category-cards .card-body h5').contains('Elements').click()  
         cy.get('#item-0 span').contains('Text Box').click()
-        cy.get('#userName').type('Maria')
+        // cy.get('#userName').type('Maria')
+    })
+})
+
+describe('second test', ()=> {
+    const expectedMenuItemNames = [
+        'Elements',
+        'Forms',
+        'Alerts, Frame & Windows',
+        'Widgets',
+        'Interactions',
+        'Book Store Application'
+    ];
+
+    it('Verification menu item names', function (){
+        cy.visit('https://demoqa.com/') 
+        cy.get ('.card').should('have.length',6). each(($el, idx) => {
+                expect  ($el.text()). to.be.equal (expectedMenuItemNames[idx]);    
+        })
     })
 })
