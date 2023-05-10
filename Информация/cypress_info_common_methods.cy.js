6c283e1c52e2577a4a788549b3c6943e6db0ae1e
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-05-10 14:45:19 +0300

    Update cypress_info_common_methods.cy.js

--- Информация/cypress_info_common_methods.cy.js
+++ Информация/cypress_info_common_methods.cy.js
@@ -7,3 +7,16 @@
 // cy.title() Выдает заголовок документа в виде строки
 // cy.pause() паузаа
 // cy.log(массаж) Распечатать сообщение в журнале команда Cypress
+
+//---‐--------------- ОБЩИЕ УТВЕРЖДЕНИЯ Common Assertyons --------------------
+// ДЛИНА length  -  cy.get('div.card-body h5').should('have.length', 6)
+// КЛАСС class -  cy.get('#demo-tab-more').should('have.class', 'disabled')
+// ЦЕНИТЬ value - cy.get('#oldSelect Menu').select('Purple').should('have.value', '4')
+// ТЕКСТОВОЕ СОДЕРЖИМОЕ text content - cy.get('div.main-header').should('have.text', 'Elements')
+
+cy.get('div.main-header').should('include.text', 'Element')
+
+cy.get('div.main-header').should('contain', 'Element') 
+// ВИДИМОСТЬ Visibility - cy.get('header a img'//(заголовок изображения)//).should('be.visible')
+//СУЩЕСТВОВАНИЕ Existence - cy.get('#result').shound('exist') -  cy.get('#результат').должен('существовать')
+// АТРИБУТ Attribute -  .should('have.attr', 'placeholder', 'Full Name') - .should('have.attr', 'заполнитель', 'Полное имя')
