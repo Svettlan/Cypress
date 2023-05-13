331c766c7d8de71a08ece89bb49bf1d86b28ed16
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-05-13 19:34:48 +0300

    Update cypress_info_common_methods.cy.js

--- Информация/cypress_info_common_methods.cy.js
+++ Информация/cypress_info_common_methods.cy.js
@@ -1,27 +1,30 @@
 //-----‐-------- ОБЩИЕ МЕТОДЫ Common methods ----------------
-// cy.visit(' url')               - посетить- сайт/страницу
-// cy.get('css selector') - получить- с помощью css селектора
-// cy.contains('text')      - содержит-
-// cy.find('css selector') - находить- Получить дочерние элементы DOM определенного селектора
-// cy.url()                Выдает текущий URL в виде строки
-// cy.title() Выдает заголовок документа в виде строки
-// cy.pause() паузаа
+
+// cy.visit(' url')                 - посетить- сайт/страницу
+// cy.get('css selector')           - получить- с помощью css селектора
+// cy.contains('text')              - содержит-
+// cy.find('css selector')          - находить- Получить дочерние элементы DOM определенного селектора
+// cy.url()                         - Выдает текущий URL в виде строки
+// cy.title()                       - Выдает заголовок документа в виде строки
+// cy.pause()                       - пауза
 // cy.log(массаж) Распечатать сообщение в журнале команда Cypress
 
 //---‐--------------- ОБЩИЕ УТВЕРЖДЕНИЯ Common Assertyons --------------------
-// ДЛИНА length  -  cy.get('div.card-body h5').should('have.length', 6)
-// КЛАСС class -  cy.get('#demo-tab-more').should('have.class', 'disabled')
-// ЦЕНИТЬ value - cy.get('#oldSelect Menu').select('Purple').should('have.value', '4')
-// ТЕКСТОВОЕ СОДЕРЖИМОЕ text content - cy.get('div.main-header').should('have.text', 'Elements')
 
-cy.get('div.main-header').should('include.text', 'Element')
+// ДЛИНА - length  -  cy.get('div.card-body h5').should('have.length', 6)
+// КЛАСС -  class -  cy.get('#demo-tab-more').should('have.class', 'disabled')
+// ЦЕНИТЬ - value - cy.get('#oldSelect Menu').select('Purple').should('have.value', '4')
+// ТЕКСТОВОЕ СОДЕРЖИМОЕ - text content - cy.get('div.main-header').should('have.text', 'Elements')
 
-cy.get('div.main-header').should('contain', 'Element') 
-// ВИДИМОСТЬ Visibility - cy.get('header a img'//(заголовок изображения)//).should('be.visible')
-//СУЩЕСТВОВАНИЕ Existence - cy.get('#result').shound('exist') -  cy.get('#результат').должен('существовать')
+// cy.get('div.main-header').should('include.text', 'Element')
+// cy.get('div.main-header').should('contain', 'Element') 
+
+// ВИДИМОСТЬ - Visibility - cy.get('header a img'//(заголовок изображения)//).should('be.visible')
+// СУЩЕСТВОВАНИЕ - Existence - cy.get('#result').shound('exist') -  cy.get('#результат').должен('существовать')
 // АТРИБУТ Attribute -  .should('have.attr', 'placeholder', 'Full Name') - .should('have.attr', 'заполнитель', 'Полное имя')
 
 //----------------------- КОМАНДЫ ДЕЙСТВИЙ Action commands -----------------------
+
 // . click()     - клик -  check() / .uncheck - проверить/снять отметку - Этот элемент должен быть <input> с флажком типа или переключателем- чекбокс или инпут
 
 // .dblclick()  - .select() Этот элемент должен иметь тег select
@@ -29,12 +32,12 @@ cy.get('div.main-header').should('contain', 'Element')
 //  .type()  - тип()
 // .clear() - 
 
-// -------------------- ПРИНУЖДЕНИЕ- Forcing ‐-----------------------
+// --------------------------- ПРИНУЖДЕНИЕ- Forcing ‐--------------------------
+
 // Вы можете передать { force: true } большинству команд действий => принудительно запустить событие элемента
 
 // Пример:
-cy.get('button').click({force: true})
-cy.get('кнопка').click({force: true})
+// cy.get('button').click({force: true})
+// cy.get('кнопка').click({force: true})
 
-принудительно активировать щелчок и все последующие события, даже если этот элемент не считается «действующим»
-
+// принудительно активировать щелчок и все последующие события, даже если этот элемент не считается «действующим»
